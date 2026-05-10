79cc7795546f354d215d2570fcdd6bb521e4e267
HP1 <basukalasurag45@gmail.com>
2026-05-10 12:31:03 +0545

    fix: all the routes with the middleware

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CartoonLabelController;
+use App\Http\Controllers\AuthController;
 
-Route::get('/',[CartoonLabelController::class,'index'])->name('index');
+Route::get('/login',[AuthController::class,'showLogin']);
+Route::post('/login',[AuthController::class,'login'])->name('login');
+Route::post('/logout',[AuthController::class,'logout'])->name('logout');
 
-Route::prefix('company')->name('company.')->group(function () {
+Route::get('/',[CartoonLabelController::class,'index'])->name('index')->middleware('auth');
+
+Route::middleware('auth')->prefix('company')->name('company.')->group(function () {
     Route::get('/add-company',[CartoonLabelController::class,'addCompany'])->name('add');
     Route::post('/store/company',[CartoonLabelController::class,'storeCompany'])->name('store');
     Route::get('/view/company',[CartoonLabelController::class,'viewCompany'])->name('show');
@@ -14,7 +19,7 @@ Route::prefix('company')->name('company.')->group(function () {
     Route::delete('/delete/company/{id}',[CartoonLabelController::class,'deleteCompany'])->name('delete');
 });
 
-Route::prefix('retail')->name('retail.')->group(function () {
+Route::middleware('auth')->prefix('retail')->name('retail.')->group(function () {
     Route::get('/add-retail',[CartoonLabelController::class,'addRetail'])->name('add');
     Route::post('/store/retail',[CartoonLabelController::class,'storeRetail'])->name('store');
     Route::get('/view/retail',[CartoonLabelController::class,'viewRetail'])->name('show');
@@ -23,8 +28,8 @@ Route::prefix('retail')->name('retail.')->group(function () {
     Route::delete('/delete/retail/{id}',[CartoonLabelController::class,'deleteRetail'])->name('delete');
 });
 
-Route::get('/print-label',[CartoonLabelController::class,'printLabel'])->name('printLabel');
-Route::post('/printLabel',[CartoonLabelController::class,'print'])->name('print.label');
+Route::get('/print-label',[CartoonLabelController::class,'printLabel'])->name('printLabel')->middleware('auth');
+Route::post('/printLabel',[CartoonLabelController::class,'print'])->name('print.label')->middleware('auth');
 
 Route::get('/scanner',function (){
     return view('scanner');
